3a8a31ad9c1ef1ec91956f435d84c0d83d3ef52b
Bouchra Akl <bushraakl1234@gmail.com>
2023-07-29 13:00:48 -0300

    Endereco.java implemented.

---
+++ src/main/java/org/desafio/Endereco.java
@@ -0,0 +1,72 @@
+package org.desafio;
+
+public class Endereco {
+
+    private String cep;
+    private String logradouro;
+    private int numero;
+    private String complemento;
+    private String bairro;
+    private String estado;
+
+    public Endereco(String cep, String logradouro,
+                    int numero, String complemento,
+                    String bairro, String estado) {
+        this.cep = cep;
+        this.logradouro = logradouro;
+        this.numero = numero;
+        this.complemento = complemento;
+        this.bairro = bairro;
+        this.estado = estado;
+    }
+
+    public Endereco(){}
+
+    public String getCep() {
+        return cep;
+    }
+
+    public void setCep(String cep) {
+        this.cep = cep;
+    }
+
+    public String getLogradouro() {
+        return logradouro;
+    }
+
+    public void setLogradouro(String logradouro) {
+        this.logradouro = logradouro;
+    }
+
+    public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getComplemento() {
+        return complemento;
+    }
+
+    public void setComplemento(String complemento) {
+        this.complemento = complemento;
+    }
+
+    public String getBairro() {
+        return bairro;
+    }
+
+    public void setBairro(String bairro) {
+        this.bairro = bairro;
+    }
+
+    public String getEstado() {
+        return estado;
+    }
+
+    public void setEstado(String estado) {
+        this.estado = estado;
+    }
+}
